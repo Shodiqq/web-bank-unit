b44de3d4266ff2702d02cf448dcad868da3cb09d
Shodiqq <sodiqvegas@gmail.com>
2022-09-22 18:37:00 +0700

    first commit

---
+++ src/views/Barcode.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container color:blue">
+    <div class=" "> Dekatkan dengan alat timbangan sampai ID mu terdeteksi. Minta bantuan Petugas jika mengalami kesulitan dalam proses ini</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
